09c8be51491ff76d0e1b7fb22b8918013102aa8c
Rumeysa Fatma Duman <rumeyse11243@gmail.com>
2024-08-16 16:48:02 +0300

    4. soru yapildi

---
+++ src/odevler/bolum01/Q004.java
@@ -0,0 +1,10 @@
+package odevler.bolum01;
+
+public class Q004 {
+    public static void main(String[] args) {
+            System.out.println("a   a^2  a^3");
+            for (int a = 1; a <= 4; a++) {
+                System.out.println(a+ "   " + (a * a) + "    " + (a * a * a));
+            }
+        }
+    }
