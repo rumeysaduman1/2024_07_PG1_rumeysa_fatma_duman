03427ba7b81e53f1cabbf80bdedc7ee6b3b67dcd
Rumeysa Fatma Duman <rumeyse11243@gmail.com>
2024-08-27 00:46:16 +0300

    6. soru yapildi

---
+++ src/odevler/bolum01/Q006.java
@@ -0,0 +1,8 @@
+package odevler.bolum01;
+
+public class Q006 {
+    public static void main(String[]args){
+        double result = 1+2+3+4+5+6+7+8+9 ;
+        System.out.println("This is result= " +result);
+    }
+}
