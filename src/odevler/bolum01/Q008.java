bf5d7d69fcd95f37eab628cfa45ad93032a0900d
Rumeysa Fatma Duman <rumeyse11243@gmail.com>
2024-08-27 17:20:27 +0300

    8. soru yapildi

---
+++ src/odevler/bolum01/Q008.java
@@ -0,0 +1,15 @@
+package odevler.bolum01;
+
+public class Q008 {
+    public static void main(String[]args){
+        double radius = 5.5;
+        double pi = 3.14;
+
+        double perimeter = 2 * radius * pi;
+        double area = radius * radius * pi;
+
+        System.out.println("Radius: " + radius);
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+    }
+}
