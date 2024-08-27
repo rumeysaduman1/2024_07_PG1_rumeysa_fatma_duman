0da12bfb2b0304fb248dc70016a2c2a98e12bdfa
Rumeysa Fatma Duman <rumeyse11243@gmail.com>
2024-08-27 17:25:30 +0300

    9. soru yapildi

---
+++ src/odevler/bolum01/Q009.java
@@ -0,0 +1,12 @@
+package odevler.bolum01;
+
+public class Q009 {
+    public  static void main(String[]args){
+    double width = 4.5;
+    double height = 7.9;
+
+    double area = width * height;
+
+    System.out.println("Area is = " + area);
+}}
+
